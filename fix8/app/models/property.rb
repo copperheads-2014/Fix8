e65f76869ff0f8f176a053eebd8755736f39ce03
Grant Hummer <gphummer@gmail.com>
2014-12-10 20:32:14 -0600

    add properties working

--- fix8/app/models/property.rb
+++ fix8/app/models/property.rb
@@ -1,2 +1,8 @@
 class Property < ActiveRecord::Base
+
+  # BUILD THIS OUT WITH CREATION VALIDATIONS
+  validates :name, presence: true
+
+  validates :address, presence: true
+
 end
--- fix8/db/schema.rb
+++ fix8/db/schema.rb
@@ -11,15 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141211001320) do
+ActiveRecord::Schema.define(version: 20141211011354) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "properties", force: true do |t|
-    t.integer "owner_id"
-    t.string  "name"
-    t.text    "address"
+    t.integer  "owner_id"
+    t.string   "name"
+    t.text     "address"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
