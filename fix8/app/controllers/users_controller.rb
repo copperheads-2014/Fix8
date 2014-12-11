e644a4a2bdf154c944596e2bd163e8510c0a293d
Ryan Jones <run0391@gmail.com>
2014-12-10 21:33:55 -0600

    added new and create to user controller

---
+++ fix8/app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      render "Success" #Change the redirect to the proper page
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation, :user_type)
+  end
+
+end
