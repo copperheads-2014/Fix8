9d909eac4e00cb6936654b01f716ed52966036c5
Ryan Jones <run0391@gmail.com>
2014-12-10 21:32:07 -0600

    added user migration and schema

--- fix8/db/schema.rb
+++ fix8/db/schema.rb
@@ -24,4 +24,13 @@ ActiveRecord::Schema.define(version: 20141211011354) do
     t.datetime "updated_at"
   end
 
+  create_table "users", force: true do |t|
+    t.string   "email"
+    t.string   "password_digest"
+    t.string   "user_type"
+    t.string   "address"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
 end
