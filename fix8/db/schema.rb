fb29d3af9b905720eb057f78d37331d57e5bb6f6
Dave Chmura <dschmura@humbledaisy.com>
2014-12-11 08:58:17 -0600

    schema updated with jobs

--- fix8/db/schema.rb
+++ fix8/db/schema.rb
@@ -11,10 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141211004207) do
+ActiveRecord::Schema.define(version: 20141211143227) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "jobs", force: true do |t|
+    t.integer  "property_id"
+    t.text     "description"
+    t.date     "start_date"
+    t.date     "end_date"
+    t.integer  "max_bid"
+    t.integer  "bid_length"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email"
