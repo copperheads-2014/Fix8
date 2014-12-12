ece60a676b8da035306446998b57f8b80534d550
Ryan Jones <run0391@gmail.com>
2014-12-11 23:03:25 -0600

    added validations into model for review

--- fix8/app/models/review.rb
+++ fix8/app/models/review.rb
@@ -1,6 +1,11 @@
 class Review < ActiveRecord::Base
+  enum review_type:  [ :for_contractor, :for_landlord ]
+
   belongs_to  :bid
 
-  enum review_type:  [ :for_contractor, :for_landlord ]
+  validates :bid_id, presence: true
+  validates :review_type, presence: true
+  validates_uniqueness_of :bid_id, scope: :review_type
+  validates :rating, :inclusion => {:in => [0,1,2,3,4,5,6,7,8,9,10]}, presence: true
 
 end
