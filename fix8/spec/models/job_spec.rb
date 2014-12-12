4c1c534404e46d7f6c0b81d89526dcfb0928f668
gphummer <gphummer@gmail.com>
2014-12-11 22:49:43 -0600

    added pry gems and working on making job tests better

--- fix8/spec/models/job_spec.rb
+++ fix8/spec/models/job_spec.rb
@@ -1,8 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Job, :type => :model do
-  let(:job) {Job.new(property_id: 1, description: 'This is a job description', start_date: '2014-12-11 08:40:43 -0600', end_date: '2014-12-11 08:40:43 -0600', max_bid: 200, bid_length: 2)}
-  describe 'Job validations' do
+  let(:job) { Job.new(property_id: 1, description: 'This is a job description', start_date: '2014-12-11 08:40:43 -0600', end_date: '2014-12-11 08:40:43 -0600', max_bid: 200, bid_length: 2) }
+  # let(:bid) { Bid.new(price: 1000, contractor_id: 1, job_id: 1, comment: 'no comment', accepted: false) }
+  # let(:bid2) { Bid.new(price: 500, contractor_id: 2, job_id: 1, comment: 'Cheap!', accepted: false) }
+  describe 'validations' do
 
     it 'requires a property id' do
       job.property_id = 1
@@ -30,4 +32,16 @@ RSpec.describe Job, :type => :model do
     end
 
   end
+
+  describe 'associations' do
+
+  it 'returns an ActiveRecord association array of bids' do
+    job.bids << Bid.new(price: 500, contractor_id: 2, comment: 'Cheap!', accepted: false)
+    # job.bids << bid2
+    expect(job.bids.count).to eq(2)
+  end
+
+
+  end
+
 end
