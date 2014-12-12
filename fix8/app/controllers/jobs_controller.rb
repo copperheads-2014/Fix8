13a62af5a769f92fd11da434099127f28f1a7899
Dave Chmura <dschmura@humbledaisy.com>
2014-12-12 14:05:41 -0600

    updated jobs controller to add create and index actions

--- fix8/app/controllers/jobs_controller.rb
+++ fix8/app/controllers/jobs_controller.rb
@@ -3,4 +3,18 @@ class JobsController < ApplicationController
     @job = Job.new
     @properties = Property.all
   end
+
+  def create
+    @job = Job.new(job_params)
+    if @job.save
+       redirect_to jobs_path
+    else
+      render :new
+    end
+  end
+
+  def index
+    @jobs = Job.all
+  end
+
 end
