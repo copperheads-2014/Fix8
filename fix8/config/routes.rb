02f480682ed860fd4270812f23bd1e74136d84cd
Ryan Jones <run0391@gmail.com>
2014-12-13 13:39:51 -0600

    added new and create routes for bids

--- fix8/config/routes.rb
+++ fix8/config/routes.rb
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
   resources :users, only: [:create, :new]
 
   resources :jobs
+  resources :bids, only: [:create, :new]
   # You can have the root of your site routed with "root"
   root 'home#index'
 
