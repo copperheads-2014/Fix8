ac290d0174f294f5b93fc2f4ed8c22469a8b5948
Ryan Jones <run0391@gmail.com>
2014-12-13 12:51:14 -0600

    added jobs a contractor can bid on list functionality

--- fix8/app/controllers/home_controller.rb
+++ fix8/app/controllers/home_controller.rb
@@ -2,11 +2,23 @@ class HomeController < ApplicationController
   before_action :set_user
 
   def index
+    if contractor?
+      @my_jobs = @user.jobs_i_can_bid_on
+      # @open_jobs = Job.all - User.
+    end
   end
 
   private
 
   def set_user
     @user ||= User.find(session[:user_id])
+  end
+
+  def landlord?
+    @user.user_type == 'landlord'
+  end
+
+  def contractor?
+    @user.user_type == 'contractor'
   end
 end
