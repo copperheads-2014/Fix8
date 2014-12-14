cd346425a1739ae4d0a106b2ad3f438f4351c6e1
Ryan Jones <run0391@gmail.com>
2014-12-13 22:59:02 -0600

    updated config, validation, and controller to allow update on user

--- fix8/config/routes.rb
+++ fix8/config/routes.rb
@@ -17,7 +17,7 @@ Rails.application.routes.draw do
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-  resources :users, only: [:create, :new, :show, :edit]
+  resources :users, only: [:create, :new, :show, :edit, :update]
 
   resources :jobs do
     resources :bids, only: [:create, :new] do
