a7ece7334d6627cc20c112a6bc6e385e12e6af55
Ryan Jones <run0391@gmail.com>
2014-12-14 20:35:58 -0600

    added validation that prevents a bid being submitted that is larger than the max bid

--- app/models/bid.rb
+++ app/models/bid.rb
@@ -7,4 +7,10 @@ class Bid < ActiveRecord::Base
   validates :job_id, presence: true
   validates :price, presence: true, numericality: true
 
+  before_validation :less_than_max_bid
+
+
+  def less_than_max_bid
+    self.job.max_bid > self.price
+  end
 end
