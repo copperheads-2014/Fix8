fd8e6b6195a6e2e8e7c2208fcc04b2a160cc5348
David Chmura & Ryan Jones <pair+dschmura.ryanbjones@devbootcamp.com>
2014-12-15 19:13:47 -0600

    added necessary lines in config file to cache assets and data

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,6 +44,8 @@ Rails.application.configure do
   # Set to :debug to see everything in the log.
   config.log_level = :info
 
+  config.cache_store = :dalli_store
+
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
 
